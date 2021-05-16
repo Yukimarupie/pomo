8120b26e39f943df6a28489fdcfbb070c5b82330
Yukimarupie <y.harada59@gmail.com>
2021-05-16 15:47:35 +0900

    set routes for heroku

---
+++ app/controllers/task_controller.rb
@@ -0,0 +1,12 @@
+class TaskController < ApplicationController
+  def index
+  end
+
+  def show; end
+
+  def edit
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'task/index'
+  get 'task/show'
+  get 'task/edit'
+  get 'task/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
