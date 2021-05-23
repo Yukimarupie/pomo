c7d264d98c525c7acbe42a0605aa5e6e5bf7d03d
Yukimarupie <y.harada59@gmail.com>
2021-05-23 10:56:53 +0900

    please add models after getting advice of ER

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,26 @@
 class TasksController < ApplicationController
-  def index; end
+  def index
+    @tasks = Task.all
+    @task = Task.new #remote: trueのために追加
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def destory
+
+  end
+
+  private
+
+  def task_params
+    # params.require(:task).permit(:name)
+  end
 end
