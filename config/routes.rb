156d2d93275b1d49c62ae8643e05ec310d93c77b
Yukimarupie <y.harada59@gmail.com>
2021-05-29 16:53:42 +0900

    WATCH THIS COMMIT for if firebase storage URL doesn't work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :tasks do
-    patch :done, on: :collection    
+    patch :done, on: :collection 
+    #collection: ルーティングにコレクション（/photos/searchのようにidを伴わないパス）を追加するときに使う https://bityl.co/7548
+    #member: idが伴う時。
   end
   root 'tasks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
