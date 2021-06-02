190a5b50d39f1d2363c883b17d805ca55e0d6fcc
Yukimarupie <y.harada59@gmail.com>
2021-06-02 18:39:10 +0900

    test to desplay <%= task.done %>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'testuser', to: 'tasks#testuser'
+
   resources :tasks do
     patch :done, on: :member 
     #collection: ルーティングにコレクション（/photos/searchのようにidを伴わないパス）を追加するときに使う https://bityl.co/7548
     #member: idが伴う時。
   end
   root 'tasks#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
